8044c457821c6b74794a176ef5f529d820fc2aa2
Jeffrey Goldbeck <jeffg5001@gmail.com>
2018-03-16 13:42:53 -0400

    add sections like image url, media type, start time, duration, caption

---
+++ client/components/addMediaForm.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { connect } from 'react-redux';
+
+var testData = {storyId:1, media:[{
+    src:'https://d3qi0qp55mx5f5.cloudfront.net/www/i/homepage/spotlight/urban-chicago-spotlight.jpg', 
+    type:"img", 
+    start:0, 
+    end:15, 
+    options:{caption:'hi'}}]
+  }
+
+const AddMediaForm = props => {
+
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <label>Image Url</label>
+            <input type="text" name={'src'}/>
+            <label>select type</label>
+            <input type="radio" name={'type'} value={"img"} id={'type1'} />
+            <label for="type1">image</label>
+            <input type="radio" name={'type'} value={"video"} id={'type2'} />
+            <label for="type2">video</label>
+            <label >start at:</label>
+            <input type="number" name={'start'} />
+            <label > Duration</label>
+            <input type="number" name={'duration'} />
+            <label > Caption (optional)</label>
+            <input type="text" name={'caption'} />
+            <button type="submit">Submit</button>
+        </form>
+    )
+}
+
+const mapState = null
+const mapDispatch = (dispatch) => ({
+    handleSubmit: event => {
+        event.preventDefault()
+        console.log({
+            storyId:1,
+            media:[{src:event.target.src.value, type:event.target.type.value, start:(+event.target.start.value), end:(+event.target.start.value) + (+event.target.duration.value), options:{caption:event.target.caption.value}}]
+        })
+    }
+})
+
+export default connect(mapState, mapDispatch)(AddMediaForm);
--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Editor from './RecordingEditor'
+import AddMediaForm from './addMediaForm'
 import {connect} from 'react-redux'
 
 /**
@@ -12,7 +13,7 @@ export const UserHome = (props) => {
   return (
     <div>
       <h3>Welcome, {email}</h3>
-
+      <AddMediaForm />
       <Editor audio="/Test_assets/funProject.mp3" />
     </div>
   )
