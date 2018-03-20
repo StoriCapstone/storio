e3e510bb876d5e2a44e7cead1fbc484b5b28a142
Jeffrey Goldbeck <jeffg5001@gmail.com>
2018-03-20 11:18:12 -0400

    fixes player

--- client/components/MediaPlayback/videoPlayer.js
+++ client/components/MediaPlayback/videoPlayer.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactPlayer from 'react-player'
 
-const VideoPlayer = () => {
+const VideoPlayer = (props) => {
 
     return (<ReactPlayer
 controls = {true} config={{
       youtube: {
         playerVars: { showinfo: 1 },
         preload: true
-      }}} url={this.props.videoUrl} onEnded = {() => this.props.storyAudio.play()} playing={true} />)
+      }}} url={props.videoUrl} onEnded = {() => props.storyAudio.play()} playing={true} />)
 
 }
 export default VideoPlayer
