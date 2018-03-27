460affb14eaf709c95ae645edb2a93a5bddf3475
Aaron Young <brainomite@gmail.com>
2018-03-27 09:16:52 -0400

    feat: put filtered users on state

--- client/components/addMembersToGroups.js
+++ client/components/addMembersToGroups.js
@@ -3,16 +3,21 @@ import { connect, } from 'react-redux';
 import axios from 'axios';
 
 class AddMembersToGroups extends React.Component {
+  //Lifecycles
   constructor(props) {
     super(props);
     this.state = {
       isLoaded: false,
       users: [],
+      selectedUsers: [],
+      filteredUsers: [],
       sortBy: 'first',
       sortAsc: true,
       searchStr: '',
     };
-    this.handleChange = this.handleChange.bind(this)
+    this.handleChange = this.handleChange.bind(this);
+    this.RenderHeader = this.RenderHeader.bind(this);
+    this.RenderBody = this.RenderBody.bind(this);
   }
   componentDidMount() {
     axios
@@ -27,98 +32,115 @@ class AddMembersToGroups extends React.Component {
         }))
       )
       .then(users => {
-        this.setState({ users, isLoaded: true, });
+        this.setState({ users, isLoaded: true, }, this.filterAndSortUsers);
       });
   }
+  // helpers
   handleChange(evt) {
-    this.setState({ searchStr: evt.target.value.toLowerCase(), });
+    this.setState(
+      { searchStr: evt.target.value.toLowerCase(), },
+      this.filterAndSortUsers
+    );
   }
-  render() {
+  filterAndSortUsers() {
+    const users = this.state.users;
     const searchStr = this.state.searchStr;
-    const sortUnicode = this.state.sortAsc ? '⬇' : '⬆';
-    const FIRST = 'First';
-    const LAST = 'Last';
-    const USERNAME = 'Username';
+
+    const filteredUsers = users
+      .filter(
+        user =>
+          (searchStr
+            ? user.lowFirst.includes(searchStr) ||
+              user.lowLast.includes(searchStr) ||
+              user.lowDisp.includes(searchStr)
+            : user)
+      )
+      .sort((userA, userB) => {
+        let aVal, bVal;
+        switch (this.state.sortBy) {
+          case 'first':
+            aVal = userA.lowFirst;
+            bVal = userB.lowFirst;
+            break;
+          case 'last':
+            aVal = userA.lowLast;
+            bVal = userB.lowLast;
+            break;
+          case 'username':
+            aVal = userA.lowDisp;
+            bVal = userB.lowDisp;
+            break;
+          default:
+            return 0;
+        }
+        if (this.state.sortAsc) {
+          if (aVal < bVal) {
+            return -1;
+          } else {
+            return 1;
+          }
+        } else if (aVal > bVal) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+    this.setState({ filteredUsers, });
+  }
+  // renders
+  render() {
+    const RenderHeader = this.RenderHeader;
+    const RenderBody = this.RenderBody;
     return (
       <div>
         <input type="search" onChange={this.handleChange} />
         {this.state.isLoaded ? (
           <table border="1">
-            <thead>
-              <tr>
-                <th>
-                  {this.state.sortBy === 'first'
-                    ? `${FIRST} ${sortUnicode}`
-                    : FIRST}
-                </th>
-                <th>
-                  {this.state.sortBy === 'last'
-                    ? `${LAST} ${sortUnicode}`
-                    : LAST}
-                </th>
-                <th>
-                  {this.state.sortBy === 'username'
-                    ? `${USERNAME} ${sortUnicode}`
-                    : USERNAME}
-                </th>
-                <th>-</th>
-                {/* empty, for `select user` */}
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.users
-                .filter(
-                  user =>
-                    (searchStr
-                      ? user.lowFirst.includes(searchStr) ||
-                        user.lowLast.includes(searchStr) ||
-                        user.lowDisp.includes(searchStr)
-                      : user)
-                )
-                .sort((userA, userB) => {
-                  let aVal, bVal;
-                  switch (this.state.sortBy) {
-                    case 'first':
-                      aVal = userA.lowFirst;
-                      bVal = userB.lowFirst;
-                      break;
-                    case 'last':
-                      aVal = userA.lowLast;
-                      bVal = userB.lowLast;
-                      break;
-                    case 'username':
-                      aVal = userA.lowDisp;
-                      bVal = userB.lowDisp;
-                      break;
-                    default:
-                      return 0;
-                  }
-                  if (this.state.sortAsc) {
-                    if (aVal < bVal) {
-                      return -1;
-                    } else {
-                      return 1;
-                    }
-                  } else if (aVal > bVal) {
-                    return -1;
-                  } else {
-                    return 1;
-                  }
-                })
-                .map(user => (
-                  <tr key={user.id}>
-                    <td>{user.firstName}</td>
-                    <td>{user.lastName}</td>
-                    <td>{user.displayName}</td>
-                    <td>select user</td>
-                  </tr>
-                ))}
-            </tbody>
+            <RenderHeader />
+            <RenderBody />
           </table>
         ) : (
           <h2>Please begin typing while the data is fetched</h2>
         )}
       </div>
+    );
+  }
+  RenderHeader() {
+    const sortUnicode = this.state.sortAsc ? '⬇' : '⬆';
+    const FIRST = 'First';
+    const LAST = 'Last';
+    const USERNAME = 'Username';
+    return (
+      <thead>
+        <tr>
+          <th>
+            {this.state.sortBy === 'first' ? `${FIRST} ${sortUnicode}` : FIRST}
+          </th>
+          <th>
+            {this.state.sortBy === 'last' ? `${LAST} ${sortUnicode}` : LAST}
+          </th>
+          <th>
+            {this.state.sortBy === 'username'
+              ? `${USERNAME} ${sortUnicode}`
+              : USERNAME}
+          </th>
+          <th>-</th>
+        </tr>
+      </thead>
+    );
+  }
+  RenderBody() {
+    return (
+      <tbody>
+        {this.state.filteredUsers.map(user => (
+          <tr key={user.id}>
+            <td>{user.firstName}</td>
+            <td>{user.lastName}</td>
+            <td>{user.displayName}</td>
+            <td>select user</td>
+          </tr>
+        ))}
+      </tbody>
     );
   }
 }
